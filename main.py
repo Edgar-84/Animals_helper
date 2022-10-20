e22a192c07a8c5460a12d836f41b5aafe8895ea9
edgar <edgartomko88@gmail.com>
2022-10-20 18:07:39 +0300

    create schedule for autorun

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
+import random
+
+import schedule
+
 from logger_settings import logger
 from settings import data, benchmark
 from vk_sender import VkRobot, SenderMessageVk
 
 
-timoshka_cat = VkRobot(token=data.token,
-                        message=data.message['timoshka'] + data.hashtags['cats_girl_hashtags'],
-                        groups_id=data.all_animals_groups_id,
-                        photo=['photo202903451_457247845, photo202903451_457247850'],
-                        name='robot_one')
-
-masha_dog = VkRobot(token=data.token,
-                        message=data.message['masha'] + data.hashtags['dog_hashtags'],
-                        groups_id=data.all_dogs_id,
-                        photo=['photo202903451_457247853, photo202903451_457247854, photo202903451_457247855'],
-                        name='robot_two')
-
-
 blacki_dog = VkRobot(token=data.token,
-                        message=data.message['blacki'] + data.hashtags['dog_hashtags'],
-                        groups_id=data.all_dogs_id,
-                        photo=['photo202903451_457247858', 'photo202903451_457247859', 'photo202903451_457247860',
-                               'photo202903451_457247861'],
-                        name='robot_two')
+                     message=data.message['blacki'] + data.hashtags['dog_hashtags'],
+                     groups_id=data.all_dogs_id,
+                     photo=['photo202903451_457247858', 'photo202903451_457247859', 'photo202903451_457247860',
+                            'photo202903451_457247861'],
+                     name='blacki_dog')
+
+
+small_cat = VkRobot(token=data.token,
+                    message=data.message['small_cat'] + data.hashtags['small_cat_hashtags'],
+                    groups_id=data.all_animals_groups_id,
+                    photo=['photo202903451_457247863', 'photo202903451_457247864', 'photo202903451_457247865',
+                           'photo202903451_457247866'],
+                    name='small_cat')
+
+
+aristocratka = VkRobot(token=data.token,
+                       message=data.message['aristocratka'] + data.hashtags['cats_girl_hashtags'],
+                       groups_id=data.all_animals_groups_id,
+                       photo=['photo202903451_457247867', 'photo202903451_457247868', 'photo202903451_457247869',
+                              'photo202903451_457247870'],
+                       name='aristocratka')
 
 
 @benchmark
@@ -44,5 +51,20 @@ def start_vk_sender(objects: list):
         logger.info(f"{'#' * 15} Finish, sent {count_successful} records from {len(animal.groups_id)} {'#' * 15}")
 
 
-if __name__ == "__main__":
-    start_vk_sender([blacki_dog])
+
+def main():
+    schedule.every().day.at(f'18:{random.randint(10, 30)}').do(start_vk_sender, [blacki_dog])
+    schedule.every().sunday.at(f'19:{random.randint(10, 30)}').do(start_vk_sender, [small_cat])
+    schedule.every().monday.at(f'19:{random.randint(10, 30)}').do(start_vk_sender, [aristocratka])
+    schedule.every().tuesday.at(f'19:{random.randint(10, 30)}').do(start_vk_sender, [small_cat])
+    schedule.every().wednesday.at(f'19:{random.randint(10, 30)}').do(start_vk_sender, [aristocratka])
+    schedule.every().thursday.at(f'19:{random.randint(10, 30)}').do(start_vk_sender, [small_cat])
+    schedule.every().friday.at(f'19:{random.randint(10, 30)}').do(start_vk_sender, [aristocratka])
+    schedule.every().saturday.at(f'19:{random.randint(10, 30)}').do(start_vk_sender, [small_cat])
+    while True:
+        schedule.run_pending()
+
+
+if __name__ == '__main__':
+    main()
+
